751447c7ecbf710ba3657f3e90790409c7c6ee9d
sky971126 <sky971126@163.com>
2018-09-22 22:59:40 -0400

    pick 1 from 20 answers

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,31 @@
+import random
+
 def ask_for_question():
 	question = input("What is your question? ")
+
+def give_an_answer():
+	return Answers[random.randint(0,19)]
+
+Answers = [	
+"It is certain.",
+"It is decidedly so.",
+"Without a doubt.",
+"Yes - definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most likely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."]
 
 ask_for_question()
